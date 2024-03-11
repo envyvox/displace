84e4b380cdb869e238bf68987b5b97e8f881bc1c
envyvox <envyvox@gmail.com>
2024-03-11 10:08:53 +0200

    fix(auth): fixed "Missing Suspense boundary with useSearchParams"

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { BuiltInProviderType } from "next-auth/providers";
 import {
@@ -20,13 +20,18 @@ import {
 } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
+const AuthError = () => {
+  const params = useSearchParams();
+  const error = params.get("error");
+
+  return error && <div className="col-span-2 text-destructive">{error}</div>;
+};
+
 const SignInPage = () => {
   const [providers, setProviders] = useState<Record<
     LiteralUnion<BuiltInProviderType, string>,
     ClientSafeProvider
   > | null>();
-  const params = useSearchParams();
-  const error = params.get("error");
 
   useEffect(() => {
     const loadData = async () => {
@@ -37,43 +42,43 @@ const SignInPage = () => {
   }, []);
 
   return (
-    <main className="container py-6">
-      <Card className="grid p-6 sm:grid-cols-2">
-        <div className="flex flex-col items-center justify-center">
-          <div>
-            <CardHeader className="space-y-1">
-              <CardTitle className="text-2xl">Войти в систему</CardTitle>
-              <CardDescription>
-                Используя один из следующих сервисов:
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid grid-cols-2 gap-5">
-              {providers ? (
-                Object.values(providers).map((provider) => (
-                  <Button
-                    key={provider.name}
-                    variant="outline"
-                    onClick={() =>
-                      signIn(provider.id, { callbackUrl: "/dashboard" })
-                    }
-                  >
-                    {provider.name}
-                  </Button>
-                ))
-              ) : (
-                <>
-                  <Skeleton className="h-10" />
-                  <Skeleton className="h-10" />
-                </>
-              )}
-              {error && (
-                <div className="col-span-2 text-destructive">{error}</div>
-              )}
-            </CardContent>
+    <Suspense>
+      <main className="container py-6">
+        <Card className="grid p-6 sm:grid-cols-2">
+          <div className="flex flex-col items-center justify-center">
+            <div>
+              <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl">Войти в систему</CardTitle>
+                <CardDescription>
+                  Используя один из следующих сервисов:
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid grid-cols-2 gap-5">
+                {providers ? (
+                  Object.values(providers).map((provider) => (
+                    <Button
+                      key={provider.name}
+                      variant="outline"
+                      onClick={() =>
+                        signIn(provider.id, { callbackUrl: "/dashboard" })
+                      }
+                    >
+                      {provider.name}
+                    </Button>
+                  ))
+                ) : (
+                  <>
+                    <Skeleton className="h-10" />
+                    <Skeleton className="h-10" />
+                  </>
+                )}
+                <AuthError />
+              </CardContent>
+            </div>
           </div>
-        </div>
-      </Card>
-    </main>
+        </Card>
+      </main>
+    </Suspense>
   );
 };
 
