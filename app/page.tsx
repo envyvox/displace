546d998d09ce4f04b60922c70ced3d900b6f296a
envyvox <envyvox@gmail.com>
2024-03-11 10:10:16 +0200

    fix(main-page): added -mt-24 to match onboarding margin

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import TypographyH1 from "@/components/typography/h1";
 
 export default function Home() {
   return (
-    <main className="container flex min-h-screen flex-col items-center justify-center space-y-12">
+    <main className="container -mt-24 flex min-h-screen flex-col items-center justify-center space-y-12">
       <TypographyH1>Displace</TypographyH1>
       <Link
         className={buttonVariants({ variant: "default" })}
