9ea6bd322e46aa23b9c71543023fb6631b941752
envyvox <envyvox@gmail.com>
2024-03-13 13:44:18 +0200

    feat(session-avatar): redesign session avatar

--- components/session-avatar.tsx
+++ components/session-avatar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { LogIn, LogOut } from "lucide-react";
+import { useUserStore } from "@/store/user-store";
+import { LogOut } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,39 +17,42 @@ import {
 import { Skeleton } from "./ui/skeleton";
 
 const SessionAvatar = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  const user = useUserStore((state) => state.user);
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Avatar className="hover:cursor-pointer">
-          <AvatarImage src={session?.user?.image ?? ""} />
-          <AvatarFallback>
-            <Skeleton className="rounded-full" />
-          </AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>
-          {session
-            ? session.user?.name + " " + session.user?.email
-            : "Не авторизован"}
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        {session ? (
-          <DropdownMenuItem onClick={() => signOut()}>
-            <LogOut className="mr-2 size-4" />
-            <span>Выйти</span>
-          </DropdownMenuItem>
-        ) : (
-          <DropdownMenuItem onClick={() => signIn()}>
-            <LogIn className="mr-2 size-4" />
-            <span>Войти</span>
-          </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+  switch (status) {
+    case "loading":
+      return <Skeleton className="size-10 rounded-full" />;
+    case "unauthenticated":
+      return (
+        <Button variant="outline" onClick={() => signIn()}>
+          Войти
+        </Button>
+      );
+    case "authenticated":
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="hover:cursor-pointer">
+              <AvatarImage src={session?.user?.image ?? ""} />
+              <AvatarFallback>
+                <Skeleton className="rounded-full" />
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>
+              {(user.handle ?? session.user?.name) + " " + session.user?.email}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => signOut()}>
+              <LogOut className="mr-2 size-4" />
+              <span>Выйти</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+  }
 };
 
 export default SessionAvatar;
