8d4173c36c027caa31c2df74c2f3a1d7a2a457b5
envyvox <envyvox@gmail.com>
2024-03-25 18:21:18 +0200

    feat(projects): added project detail stack skeleton

--- components/project-card/project-detail-stack.tsx
+++ components/project-card/project-detail-stack.tsx
@@ -1,4 +1,5 @@
 import { Card, CardHeader } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import Bagdes from "@/components/bagdes";
 import TypographyLarge from "@/components/typography/large";
 
@@ -8,15 +9,31 @@ type Props = {
 };
 
 const ProjectDetailStack = ({ stack, isLoading }: Props) => {
-  // TODO: add skeleton on loading
   return (
     <Card className="h-fit md:max-w-md">
       <CardHeader>
-        <TypographyLarge>Используемые технологии</TypographyLarge>
-        <Bagdes
-          values={stack.map((s, index) => ({ key: s + index, value: s }))}
-          displayCount={Infinity}
-        />
+        {isLoading ? (
+          <>
+            <Skeleton className="h-[24px] w-2/3" />
+            <div className="flex flex-wrap gap-2">
+              <Skeleton className="h-[22px] w-16 rounded-full" />
+              <Skeleton className="h-[22px] w-16 rounded-full" />
+              <Skeleton className="h-[22px] w-16 rounded-full" />
+              <Skeleton className="h-[22px] w-16 rounded-full" />
+              <Skeleton className="h-[22px] w-16 rounded-full" />
+              <Skeleton className="h-[22px] w-16 rounded-full" />
+              <Skeleton className="h-[22px] w-16 rounded-full" />
+            </div>
+          </>
+        ) : (
+          <>
+            <TypographyLarge>Используемые технологии</TypographyLarge>
+            <Bagdes
+              values={stack.map((s, index) => ({ key: s + index, value: s }))}
+              displayCount={Infinity}
+            />
+          </>
+        )}
       </CardHeader>
     </Card>
   );
